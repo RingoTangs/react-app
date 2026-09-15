d16d035211f5420ec7e7ac63ed0a1e7908bc8217
RingoTangs <singrainyang@gmail.com>
2026-09-15 18:19:52 +0800

    refactor: format eslint configuration for improved readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
 import antfu from '@antfu/eslint-config'
 import prettier from 'eslint-config-prettier'
 
-export default antfu({
-  type: 'app',
-  react: true,
-  typescript: true,
-  stylistic: false,
-  formatters: false,
-  gitignore: true,
-  test: true,
-  ignores: ['**/routeTree.gen.ts', '**/pnpm-lock.yaml'],
-})
-  .append({
+export default antfu(
+  {
+    type: 'app',
+    react: true,
+    typescript: true,
+    stylistic: false,
+    formatters: false,
+    gitignore: true,
+    test: true,
+    ignores: ['**/routeTree.gen.ts', '**/pnpm-lock.yaml'],
+  },
+  {
     files: ['src/routes/**/*.{ts,tsx}'],
     rules: {
       'react-refresh/only-export-components': 'off',
     },
-  })
-  .append(prettier)
+  },
+).append(prettier)

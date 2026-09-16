aaac7f0dc390597cfc84060d130bdba215b22cda
RingoTangs <singrainyang@gmail.com>
2026-09-16 21:51:23 +0800

    fix: rename ErrorRouteDemo to ErrorDemoPage and update error message for clarity

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-const ErrorRouteDemo: React.FC = () => {
-  throw new Error('1')
+const ErrorDemoPage: React.FC = () => {
+  throw new Error('Intentional error for testing the route error boundary.')
 }
 
 export const Route = createFileRoute('/error')({
-  component: ErrorRouteDemo,
+  component: ErrorDemoPage,
 })

13fd85f6a06bea5358308ca2b83f1751635d0ea1
RingoTangs <singrainyang@gmail.com>
2026-09-19 10:49:57 +0800

    feat: update import paths for LoaderProgress and LoaderSpin components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { LoaderProgress } from '@/app/LoaderProgress'
+import { LoaderSpin } from '@/app/LoaderSpin'
 import { queryClient } from '@/app/queryClient'
 import { router } from '@/app/router'
-import { LoaderProgress } from './app/LoaderProgress'
-import { LoaderSpin } from './app/LoaderSpin'
 
 const App: React.FC = () => {
   return (

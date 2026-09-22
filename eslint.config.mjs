5a484190a78ed93442fae98eb6ad020055e90101
RingoTangs <singrainyang@gmail.com>
2026-09-22 22:21:25 +0800

    refactor: simplify ESLint configuration by consolidating rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,16 @@
 import antfu from '@antfu/eslint-config'
 import prettier from 'eslint-config-prettier'
 
-export default antfu(
-  {
-    type: 'app',
-    react: true,
-    typescript: true,
-    stylistic: false,
-    formatters: false,
-    gitignore: true,
-    test: true,
-    ignores: ['**/routeTree.gen.ts', '**/pnpm-lock.yaml'],
+export default antfu({
+  type: 'app',
+  react: true,
+  typescript: true,
+  stylistic: false,
+  formatters: false,
+  gitignore: true,
+  test: true,
+  ignores: ['**/routeTree.gen.ts', '**/pnpm-lock.yaml'],
+  rules: {
+    'react-refresh/only-export-components': 'off',
   },
-  {
-    files: ['src/routes/**/*.{ts,tsx}', 'src/components/**/*.{ts,tsx}'],
-    rules: {
-      'react-refresh/only-export-components': 'off',
-    },
-  },
-).append(prettier)
+}).append(prettier)
